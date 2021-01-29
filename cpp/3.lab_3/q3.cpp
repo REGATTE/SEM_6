26d98fdf2a6930cef058e3b02298d5bf68a5f803
Ashok <ashok_a380@icloud.com>
2021-01-29 10:28:16 +0530

    C++ lab3 q3

--- cpp/3.lab_3/q3.cpp
+++ cpp/3.lab_3/q3.cpp
@@ -1,18 +1,12 @@
+
 #include <iostream>;
+#include <cmath>
 using namespace std;
 int main(){
     int x;
     std::cout << "enter the value of record";
     std::cin >> x;
-    int count = 0;
-    for (int i = 1; i = x+1; i++){
-        if(i == 1){
-            count = count + 1;
-        }
-        if(i == x){
-            count = count + 1;
-        }
-
-    }
+    int count = pow(x,2) - 1;
+    cout << count;
     return 0;
 }
